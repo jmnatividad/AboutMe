3d0880ec2c0bae6e1a5d8a04c771b80c4ec5cd2a
Jeric Marx R. Natividad <jericmarxnatividad@gmail.com>
2024-01-05 15:42:44 +0800

    1

--- app.js
+++ app.js
@@ -3,48 +3,55 @@
 const ulAbout = document.querySelector('.ul-about');
 
 const data = [
-    {
-        "Title" : "Visit Camsur",
-        "Date" : "2023",
-        "Description" : "Visit Camsur is a mobile application developed using React Native and Supabase as the backend, Colaboratively working in a team handling the Front end development and Api integration of facebook and Google for the acount creation. Visit Camsur is an app that aims to showcase the beaty of each tourist spots in Camarine sur.",
-        "Overview" : "Dev 2022 - april 2023. Mobile application. Typescript, Tailwind css, Supabase, React Nativd and Supabase.",
-        "image" : "Img/FB.png"
-    },
+    
     {
         "Title" : "Trifecta Exacta",
         "Date" : "2023",
         "Description" : "Trifecta Exacta is a prototype game developed using Unity 3d engine. Working as the development lead for the project and handled programming of the physics and events in the game, also troubleshooting and quality testing the game.",
         "Overview" : "Jul 2018 - Mar 2019. 2D PC game. C#, Unity Engine.",
-        "image" : "Img/FB.png"
+        "image1" : "",
+        "image2" : ""
     },
     {
         "Title" : "Jeric Flower shop website",
         "Date" : "2023",
         "Description" : "This website is personal project that is a ecomerce type website developed using Laravel and Mysql as the backend. Fullstack developing the website and overseeing the perfect design for the website output",
         "Overview" : "2023. Website . Php, Mysql, Javascript, Laravel.",
-        "image" : "Img/FB.png"
-    },
-    {
-        "Title" : "Adventure of Anatoly Sokolov",
-        "Date" : "2019",
-        "Description" : "Adventure of Anatoly Sokolov is a 3d side scrolling game, it was developed using Unity 3D. Handled all the programming for the controlls and events in the game. Assets are from a unity assets store and are free.",
-        "Overview" : "Aug - December, 2019. 3D PC game. C#, Unity Engine.",
-        "image" : "Img/Portfolio/Games/Adventure_Menu.png"
-    },
-    {
-        "Title" : "Operation Time",
-        "Date" : "2019",
-        "Description" : "Operation time is a 2D game developed with Unity engine. It is a fighting game that has a story and stages for enemies. Developed the entire game and asssets to be used in it and the codes for the events and functions for the game ",
-        "Overview" : "Jul 2018 - Mar 2019. 2D mobile game. C#, Unity Engine.",
-        "image" : "Img/FB.png"
+        "image1" : "",
+        "image2" : ""
     },
     {
         "Title" : "Mystery Camsur",
         "Date" : "2022",
         "Description" : "Mystery Camsur is a 2d game developed using unity engine using C#, it is a finding the missing object game where the player is given a set of missing objects in silhouette and has a timer to find all the missing objects. Handled all the programming for the codes and events, worked with a graphic designer for the in game assets.",
         "Overview" : "2022 - 2023. 2D mobile game. C#, Unity Engine.",
-        "image" : "Img/FB.png"
-    }
+        "image1" : "Img/Portfolio/Games/MysteryCamsurMenu.png",
+        "image2" : "Img/Portfolio/Games/MysteryCamsurGame.png",
+    },
+    {
+        "Title" : "Visit Camsur",
+        "Date" : "2022",
+        "Description" : "Visit Camsur is a mobile application developed using React Native and Supabase as the backend, Colaboratively working in a team handling the Front end development and Api integration of facebook and Google for the acount creation. Visit Camsur is an app that aims to showcase the beaty of each tourist spots in Camarine sur.",
+        "Overview" : "Dec 2022 - april 2023. Mobile application. Typescript, Tailwind css, Supabase, React Nativd and Supabase.",
+        "image1" : "Img/Portfolio/MobileApp/VisitCamsur.png",
+        "image2" : ""
+    },
+    {
+        "Title" : "Adventure of Anatoly Sokolov",
+        "Date" : "2019",
+        "Description" : "Adventure of Anatoly Sokolov is a 3d side scrolling game, it was developed using Unity 3D. Handled all the programming for the controlls and events in the game. Assets are from a unity assets store and are free.",
+        "Overview" : "Aug - December, 2019. 3D PC game. C#, Unity Engine.",
+        "image1" : "Img/Portfolio/Games/Adventure_Menu.png",
+        "image2" : "",
+    },
+    {
+        "Title" : "Operation Time",
+        "Date" : "2019",
+        "Description" : "Operation time is a 2D game developed with Unity engine. It is a fighting game that has a story and stages for enemies. Developed the entire game and asssets to be used in it and the codes for the events and functions for the game ",
+        "Overview" : "Jul 2018 - Mar 2019. 2D mobile game. C#, Unity Engine.",
+        "image1" : "",
+        "image2" : ""
+    },
     
 ];
 
@@ -52,7 +59,7 @@ data.forEach(entry => {
     ulAbout.insertAdjacentHTML('beforeend', `
         <li>
             <div class="About-Me">
-                <span class="date" id="date">${entry.Date}</span>
+                <div class="date" id="date">${entry.Date}</div>
                 <div class="box boxhide">
                     <div class="about-item">
                         <span class="text-about1">${entry.Title}</span>
@@ -60,7 +67,8 @@ data.forEach(entry => {
                         <span class="text-about3">${entry.Overview}</span>
                     </div>
                     <div class="About-image">
-                        <div><img src="${entry.image}" alt="" class="" id="image"></div>
+                        <div><img src="${entry.image1}" alt="" class="box-img" id="image"></div>
+                        <div><img src="${entry.image2}" alt="" class="box-img" id="image"></div>
                     </div>
                 </div>
             </div>
@@ -145,10 +153,7 @@ window.addEventListener('scroll', function() {
     }
 });
 
-const windowWidth = window.innerWidth;
 
-// Log the window width
-console.log('Window width:', windowWidth);
 
 
 
